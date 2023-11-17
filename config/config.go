5b0658468a1063ad548bec62b999241d87157203
Ikhlash Mulyanurahman <94058040+Ikhlashmulya@users.noreply.github.com>
2023-11-17 09:01:45 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -8,26 +8,10 @@ import (
 	"github.com/spf13/viper"
 )
 
-// type Config struct {
-// }
-
-// func NewConfig(filenames ...string) *Config {
-// 	err := godotenv.Load(filenames...)
-// 	exception.PanicIfError(err)
-
-// 	return &Config{}
-// }
-
-// func (config *Config) Get(key string) string {
-// 	return os.Getenv(key)
-// }a
-
-func NewConfig(pathS string) *viper.Viper {
+func NewConfig(pathString string) *viper.Viper {
 	viper := viper.New()
 	viper.SetConfigFile("config.json")
-	viper.AddConfigPath(pathS)
-
-	fmt.Println(path.Join(pathS, "config.json"))
+	viper.AddConfigPath(pathString)
 
 	err := viper.ReadInConfig()
 	exception.PanicIfError(err)
